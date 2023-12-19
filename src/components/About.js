f67c9fb172453c6f2188e8303fb8998f04195271
ninad-debug <ninadsamant07@gmail.com>
2023-12-19 16:30:15 +0530

    my personal projects updated

---
+++ src/components/About.js
@@ -0,0 +1,51 @@
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+        <p className="text-xl mt-5">Hi there,</p>
+        <br />
+        <p className="text-xl">
+          Thanks for taking the time to visit my profile. I am a highly
+          motivated and results-driven software engineer. My passion for web
+          technologies and my commitment to making the web a better and more
+          accessible place drive me to create innovative and user-friendly web
+          applications.
+        </p>
+        <br />
+        <p className="text-xl">
+          With expertise in modern web technologies such as HTML5, CSS3,
+          JavaScript, ES6, React, and Tailwind, I have a strong foundation in
+          creating engaging and visually appealing applications. I am passionate
+          about staying up-to-date with the latest web development trends and
+          best practices, and I have a proven track record of delivering
+          high-quality projects on time and within budget.
+        </p>
+        <br />
+        <p className="text-xl">
+          My experience and technical skills have enabled me to contribute to a
+          variety of projects, and I am confident in my ability to work with
+          cross-functional teams to achieve project goals. I am dedicated to
+          continuously learning and growing my skills, and I am always looking
+          for new opportunities to challenge myself and drive results.
+        </p>
+        <br />
+        <p className="text-xl">
+          If you're looking for a talented and motivated software engineer who
+          is passionate about web technologies, I would be thrilled to discuss
+          opportunities with you. Thank you for considering my profile, and I
+          look forward to connecting soon!
+        </p>
+        <br />
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Portfolio.js
@@ -0,0 +1,72 @@
+import taskMate from "../assets/portfolio/taskMate.png";
+import cineMate from "../assets/portfolio/cineMate.png";
+import shopMate from "../assets/portfolio/shopMate.png";
+
+const Portfolio = () => {
+  const portfolios = [
+    {
+      id: 1,
+      src: taskMate,
+      demo: "https://netlify-taskmate-react-app.netlify.app/",
+      code: "https://github.com/ninad-debug/Taskmate",
+    },
+    {
+      id: 2,
+      src: cineMate,
+      demo: "https://cinemate-data.netlify.app/",
+      code: "https://github.com/ninad-debug/cinemate",
+    },
+    {
+      id: 3,
+      src: shopMate,
+      demo: "https://shopmate-ul-cr.netlify.app/",
+      code: "https://github.com/ninad-debug/shopmate-cr",
+    },
+  ];
+
+  return (
+    <div
+      name="portfolio"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Portfolio
+          </p>
+          <p className="py-6">Check out some of my work right here</p>
+        </div>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          {portfolios.map(({ id, src, demo, code }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              <img
+                src={src}
+                alt="ProjectLogo"
+                className="rounded-md duration-200 hover:scale-105"
+              />
+
+              <div className="flex items-center justify-center">
+                <a
+                  href={demo}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Demo
+                </a>
+                <a
+                  href={code}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Code
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
